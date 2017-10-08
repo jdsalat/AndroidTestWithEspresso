c82682c7b9087a10a6b9aaaed9538f98d1092d78
jdsalat <javedsalat@gmail.com>
2017-10-08 19:17:58 +0530

    Project Commit

---
+++ EspressoTestDemo/app/src/androidTest/java/salat/javed/espressotestdemo/MainActivityTest.java
@@ -0,0 +1,26 @@
+package salat.javed.espressotestdemo;
+
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Javed.Salat on 10/8/2017.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public final ActivityRule<MainActivity> main = new ActivityRule<>(MainActivity.class);
+
+    @Test
+    public void shouldLaunchMainScreen() {
+        onView(withText("Hello World!")).check(ViewAssertions.matches(isDisplayed()));
+    }
+}
